79161b25b36da00acdb945f9134a0f6466455602
lennijusten <65619456+lennijusten@users.noreply.github.com>
2021-05-14 12:57:54 -0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,10 +4,12 @@ from PIL import Image
 import numpy as np
 import os
 import argparse
+from math import floor
 
-img_shape = (224,224,3)
+img_shape = (224, 224, 3)
 
 classes = ['Amblyomma americanum', 'Dermacentor variabilis', 'Ixodes scapularis']
+
 
 def read_args():  # Get user arguments
     parser = argparse.ArgumentParser()
@@ -21,15 +23,16 @@ def read_args():  # Get user arguments
                         type=str,
                         default=None,
                         help="Save destination for results csv")
-    
+
     parser.add_argument("model",
                         type=str,
                         default=None,
                         help="Path to save model directory")
-    
+
     args = parser.parse_args()
-    
+
     return args
+
 
 args = read_args()
 
@@ -61,28 +64,25 @@ def im_crop(image):  # Crop images into a square along their shortest side
 images = []
 fname = []
 for f in os.listdir(args.source):
-        try:
-            img = get_input(os.path.join(args.source, f))
-            img_cropped = im_crop(img)
-            img_resized = img_cropped.resize((img_shape[0], img_shape[1]))
-            pixels = np.asarray(img_resized)  # convert image to array
-            pixels = pixels.astype('float32')
-            input = np.expand_dims(pixels, axis=0)  # adds batch dimension
-            images.append(input)
-            fname.append(f)
-        except:
-            print("Image {} could not be opened".format(f))
-            pass
+    try:
+        img = get_input(os.path.join(args.source, f))
+        img_cropped = im_crop(img)
+        img_resized = img_cropped.resize((img_shape[0], img_shape[1]))
+        pixels = np.asarray(img_resized)  # convert image to array
+        pixels = pixels.astype('float32')
+        input = np.expand_dims(pixels, axis=0)  # adds batch dimension
+        images.append(input)
+        fname.append(f)
+    except:
+        print("Image {} could not be opened".format(f))
+        pass
 
-   
- # stack up images list to pass for prediction              
+# stack up images list to pass for prediction
 images = np.vstack(images)
-    
+
 model = tf.keras.models.load_model(args.model)
 
-
 results = model.predict(images, batch_size=32, verbose=1)
-
 
 class_prob = np.amax(results, 1).tolist()
 rounded_class_prob = [round(100 * x, 2) for x in class_prob]
@@ -90,13 +90,14 @@ class_ind = np.argmax(results, 1)
 preds = [classes[i] for i in class_ind]
 
 NN_dict = {
-        "fname": fname,
-        "prediction": preds,
-        "class": class_ind.tolist(),
-        "prob": rounded_class_prob,
-        "results": results.tolist()
-    }
+    "fname": fname,
+    "prediction": preds,
+    "class": class_ind.tolist(),
+    "prob": rounded_class_prob,
+    "results": results.tolist()
+}
 
 df = pd.DataFrame(NN_dict)
 
-df.to_csv(os.path.join(args.dest,'results.csv'))    # save results to csv file
+df.to_csv(os.path.join(args.dest, 'results.csv'))  # save results to csv file
+
